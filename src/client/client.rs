c65f1810a359cd13bea2891d7dd05d353391267a
See2et / しーぜっと <me.see2et@gmail.com>
2024-12-15 15:27:37 +0900

    add simple scene

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,11 +1,11 @@
 use bevy::prelude::*;
 
-use super::window::window::WindowPlugin;
+use super::{scene::scene::ScenePlugin, window::window::WindowPlugin};
 
 pub struct ClientPlugin;
 
 impl Plugin for ClientPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(WindowPlugin);
+        app.add_plugins(WindowPlugin).add_plugins(ScenePlugin);
     }
 }
