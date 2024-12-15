5cbf094501c8c8187434ab66d235d85307e83f17
See2et / しーぜっと <me.see2et@gmail.com>
2024-12-15 15:03:26 +0900

    add window plugin

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,7 +1,11 @@
 use bevy::prelude::*;
 
+use super::window::window::WindowPlugin;
+
 pub struct ClientPlugin;
 
 impl Plugin for ClientPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_plugins(WindowPlugin {});
+    }
 }
---
+++ src/client/window/window.rs
@@ -0,0 +1,17 @@
+use bevy::prelude::*;
+use bevy::window::{PrimaryWindow, WindowMode, WindowResolution};
+
+pub struct WindowPlugin {}
+
+impl Plugin for WindowPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(PreStartup, init_window);
+    }
+}
+
+fn init_window(mut windows: Query<&mut Window, With<PrimaryWindow>>) {
+    if let Ok(mut window) = windows.get_single_mut() {
+        window.resolution = WindowResolution::new(1920.0, 1080.0);
+        window.mode = WindowMode::BorderlessFullscreen(MonitorSelection::Primary);
+    }
+}
