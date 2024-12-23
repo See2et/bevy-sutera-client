aa817fd24de515d7e78a1a930688ba6843913feb
See2et / しーぜっと <me.see2et@gmail.com>
2024-12-24 01:00:55 +0900

    client

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,11 +1,17 @@
 use bevy::prelude::*;
 
-use super::{scene::scene::ScenePlugin, window::window::WindowPlugin};
+use super::{
+    cursor::cursor::CursorPlugin, player::player::PlayerPlugin, scene::scene::ScenePlugin,
+    window::window::WindowPlugin,
+};
 
 pub struct ClientPlugin;
 
 impl Plugin for ClientPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(WindowPlugin).add_plugins(ScenePlugin);
+        app.add_plugins(WindowPlugin)
+            .add_plugins(ScenePlugin)
+            .add_plugins(CursorPlugin)
+            .add_plugins(PlayerPlugin);
     }
 }
--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,3 +1,5 @@
 pub mod client;
+pub mod cursor;
+pub mod player;
 pub mod scene;
 pub mod window;
